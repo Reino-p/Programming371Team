95bcbd0182bb8d8fa91b01140807174265e613f2
Reino-p <reinopowell61@gmail.com>
2023-08-15 18:56:17 +0200

    commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        //Now we will start from scratch
-        //add a comment
+        //commit and push
+        
     }
 }
