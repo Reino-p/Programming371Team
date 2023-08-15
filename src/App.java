2d68f4015b1d6058d6a59c151595f78d063ced13
Reino-p <reinopowell61@gmail.com>
2023-08-15 10:55:14 +0200

    Scratch

--- src/App.java
+++ src/App.java
@@ -1,14 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        Student one = new Student("Reino",1);
-        one.Print();
-
-        //added a new comment at end!
-
-        //removed all lines
-        //Cool
+        
+        //Now we will start from scratch
 
     }
 }
