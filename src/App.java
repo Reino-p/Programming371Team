01e300275b9cb68029272f810f433a2476ca1b17
Reino-p <reinopowell61@gmail.com>
2023-08-15 11:13:59 +0200

    added line in eclipse

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         //Now we will start from scratch
-        //added comment from netbeans
+        //added line in eclipse
     }
 }
